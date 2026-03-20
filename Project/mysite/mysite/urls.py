92086a4f2b14f5e46c84a97d3bdebfd215ee2738
Vivek Ranjan Sahoo <vivek.ncc19@gmail.com>
2026-03-20 16:54:39 +0530

    2026-03-20_Thurs

--- Project/mysite/mysite/urls.py
+++ Project/mysite/mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('blog/',include('blog.urls',namespace='blog'))
+    # path('',include('blog.urls',namespace='blog'))
+
 ]
