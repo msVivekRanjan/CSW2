b8074999a88c69c5122fa47143d13eab4c9378d0
Vivek Ranjan Sahoo <vivek.ncc19@gmail.com>
2026-03-28 16:02:52 +0530

    2026.03.28_Sat

--- Project/mysite/blog/views.py
+++ Project/mysite/blog/views.py
@@ -5,6 +5,8 @@ from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
 from django.views.generic import ListView
 from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 def post_share(request,post_id):
     post = get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
@@ -54,17 +56,21 @@ class PostListView(ListView):
     template_name='blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request,tag_slug=None):
     post_list=Post.published.all()
     paginator=Paginator(post_list,1)
     page_number=request.GET.get('page',1)
+    tag=None
+    if tag_slug:
+        tag=get_object_or_404(Tag,slug=tag_slug)
+        post_list=post_list.filter(tags__in=[tag])
     try:
         posts=paginator.page(page_number)
     except PageNotAnInteger:
         posts=paginator.page(1)
     except EmptyPage:
         posts=paginator.page(paginator.num_pages)
-    return render(request,'blog/post/list.html',{'posts':posts})
+    return render(request,'blog/post/list.html',{'posts':posts,'tag':tag})
 
 
     
@@ -79,8 +85,17 @@ def post_detail(request,year,month,day,post):
         publish__day=day
     )
     comments=post.comments.filter(active=True)
+    # Form for users to comment
     form=CommentForm()
-    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'form':form})
+    # List of similar posts
+    post_tags_ids=post.tags.values_list('id',flat=True)
+    similar_posts=Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts=similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags','-publish')[:4]
+    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'form':form,'similar_posts':similar_posts})
 
 @require_POST
 def post_comment(request,post_id):
