459652f65ef1a149da93cab809a40af6b5a5d817
Vivek Ranjan Sahoo <vivek.ncc19@gmail.com>
2026-05-02 16:57:21 +0530

    2026.05.02_SAT

--- Project/Ch4/bookmarks/bookmarks/asgi.py
+++ Project/Ch4/bookmarks/bookmarks/asgi.py
@@ -1,7 +1,8 @@
 """
 ASGI config for bookmarks project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named
+``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
@@ -11,6 +12,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmarks.settings')
 
 application = get_asgi_application()
--- Project/Ch4/bookmarks/bookmarks/wsgi.py
+++ Project/Ch4/bookmarks/bookmarks/wsgi.py
@@ -1,7 +1,8 @@
 """
 WSGI config for bookmarks project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named
+``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
@@ -11,6 +12,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmarks.settings')
 
 application = get_wsgi_application()
